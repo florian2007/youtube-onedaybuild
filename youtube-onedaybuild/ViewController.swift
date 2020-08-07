413817c54fd5c1a67aad92522f86a616e862bbfb
florian2007 <69194206+florian2007@users.noreply.github.com>
2020-08-07 17:38:09 +0100

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Florian Nurcja on 07/08/2020.
+//  Copyright © 2020 Florian Nurcja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
